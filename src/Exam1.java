c34136801bfacf6f0e9e342b73d7913d49da5ea9
meghana <sirgamega@gmail.com>
2024-12-12 00:14:41 +0530

    2nd commit

--- src/Exam1.java
+++ src/Exam1.java
@@ -4,6 +4,10 @@ public class Exam1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi");
+		int a=10;
+		int b=10;
+		int c= a+b;
+		System.out.println("Value of c is : "+c);
 	}
 
 }
